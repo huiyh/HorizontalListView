14442baffb05b0f83a40ef509ac863496480edde
huiyh <huiyhs@gmail.com>
2014-01-15 09:57:44 +0800

    添加HorizontalListView

---
+++ AndroidHorizontalListViewSample/gen/com/meetme/android/horizontallistview/sample/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.meetme.android.horizontallistview.sample;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
